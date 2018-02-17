5152c62da094e8c74fa1876f60217e468be67bbb
impu1se <impu1se@bk.ru>
2018-02-17 20:00:52 +0300

    idnw

--- coursera.py
+++ coursera.py
@@ -2,73 +2,78 @@
 from lxml import etree
 from random import shuffle
 from bs4 import BeautifulSoup
+from openpyxl import Workbook
+import argparse
 
 
-def get_courses_list(url, amount_of_cources):
-	courses_list = []
-	url = url
-	row_course_in_xml = requests.get(url).content
-	root_xml = etree.fromstring(row_course_in_xml)
-	for link in root_xml.iter():
-		if 'loc' in link.tag:
-			courses_list.append(link.text)
-	shuffle(courses_list)
-	return courses_list[:amount_of_cources]
+def get_courses_list(url, amount_of_course):
+    courses_list = []
+    row_course_in_xml = requests.get(url).content
+    root_xml = etree.fromstring(row_course_in_xml)
+    for link in root_xml.iter():
+        if 'loc' in link.tag:
+            courses_list.append(link.text)
+    shuffle(courses_list)
+    return courses_list[:amount_of_course]
 
 
 def get_course_info(course_slug):
-	for course in course_slug:
-		response_content = requests.get(course)
-		response_content.encoding = 'utf-8'
-		raw_content = response_content.text	
-		parsing_course = BeautifulSoup(raw_content, 'html.parser')
-		course_name = parsing_course.h1.text
-		course_language = parsing_course.find('div', 'rc-Language').text
-		start_date = parsing_course.find('div', 'startdate')
-		if start_date == None:
-			start_date = 'No info'
-		else:
-			start_date = start_date.text 
-		course_week = parsing_course.find('div', 'rc-WeekView')
-		if course_week == None:
-			course_week = 'No info'
-		else:
-			course_week = len(course_week)
-		ratings = parsing_course.find('div', 'ratings-text')
-		if ratings == None:
-			ratings = 'No info'
-		else:
-			ratings = ratings.text
-		print(f"""Course name : {course_name}, \n \
-			language: {course_language} , \n \
-			start: {start_date},  \n \
-			week: {course_week}  , \n \
-			rate: {ratings}""")
+    courses_info = {}
+    for course in course_slug:
+        response_content = requests.get(course)
+        response_content.encoding = 'utf-8'
+        raw_content = response_content.text
+        parsing_course = BeautifulSoup(raw_content, 'html.parser')
+        course_name = parsing_course.h1.text
+        course_language = parsing_course.find('div', 'rc-Language').text
+        start_date = parsing_course.find('div', 'startdate')
+        if start_date is None:
+            start_date = 'No info'
+        else:
+            start_date = start_date.text
+        amount_week = parsing_course.find('div', 'rc-WeekView')
+        if amount_week is None:
+            amount_week = 'No info'
+        else:
+            amount_week = len(amount_week)
+        ratings = parsing_course.find('div', 'ratings-text')
+        if ratings is None:
+            ratings = 'No info'
+        else:
+            ratings = ratings.text
+        courses_info.update({
+        	course: {
+            'course name': course_name,
+            'course language': course_language,
+            'start date': start_date,
+            'number of week': amount_week,
+            'ratings': ratings,
+        }})
+    return courses_info
 
 
-def output_courses_info_to_xlsx(filepath):
-    pass
+def output_courses_info_to_xlsx(courses_info):
+    xls_file = Workbook()
+    sheet = xls_file.active
+    for row, course in enumerate(courses_info, start=1):
+        for column, description in enumerate(courses_info[course], start=1):
+            sheet.cell(
+                row=row,
+                column=column).value = courses_info[course][description]
+    xls_file.save('courses_info.xlsx')
+
+
+def main():
+    argument = argparse.ArgumentParser(description='Amount courses')
+    argument.add_argument('-c', dest='courses', default=20, type=int,
+    	help='Input amount courses you need')
+    args = argument.parse_args()
+    print(args.courses)
+    url = 'https://www.coursera.org/sitemap~www~courses.xml'
+    courses_list = get_courses_list(url, args.courses)
+    course_info = get_course_info(courses_list)
+    output_courses_info_to_xlsx(course_info)
 
 
 if __name__ == '__main__':
-    url = 'https://www.coursera.org/sitemap~www~courses.xml'
-    result = get_courses_list(url, 20)
-    get_course_info(result)
-
-
-
-
-
-# def get_courses_list(amount_of_cources):
-#    url = "https://www.coursera.org/sitemap~www~courses.xml"
-#    coursera_courses_responce = requests.get(url)  
-#    coursera_courses_xml_as_bytecode = coursera_courses_responce.content
-#    urlset_xml_root_object = objectify.fromstring(coursera_courses_xml_as_bytecode)
-#    full_courses_list = [
-#        url_object.loc.text
-#        for url_object in urlset_xml_root_object.iterchildren()
-#    ]
-#    cources_links_list = random.sample(full_courses_list, amount_of_cources)
-#    print(cources_links_list)
-#    return cources_links_list
-
+    main()
